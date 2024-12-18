78753b985c95e6d894eb1b0ab7de567b5d9dca37
Jan-Olav Eide <jan-olav,eide@nav.no>
2024-12-18 10:03:39 +0100

    enable probes

--- src/main/kotlin/no/nav/helse/navepjauthserver/NavEpjAuthServerApplication.kt
+++ src/main/kotlin/no/nav/helse/navepjauthserver/NavEpjAuthServerApplication.kt
@@ -3,8 +3,11 @@ package no.nav.helse.navepjauthserver
 import no.nav.helse.navepjauthserver.Cluster.Companion.profiler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
-@SpringBootApplication class NavEpjAuthServerApplication
+@SpringBootApplication
+@EnableWebSecurity
+class NavEpjAuthServerApplication
 
 fun main(args: Array<String>) {
     runApplication<NavEpjAuthServerApplication>(*args){

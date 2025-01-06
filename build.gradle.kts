7221345f70a2cdb246ad4aa80654fae2f4ab62f8
Leo-Andreas Ervik <6738905+leoervik@users.noreply.github.com>
2025-01-06 12:49:58 +0100

    [wip] login with helseid ok, callback not ok

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,11 +25,15 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springframework.security:spring-security-oauth2-jose")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("net.logstash.logback:logstash-logback-encoder:8.0")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("com.nimbusds:nimbus-jose-jwt:9.47")
+
   developmentOnly("org.springframework.boot:spring-boot-devtools")
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 
--- src/main/kotlin/no/nav/helse/navepjauthserver/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/helse/navepjauthserver/security/SecurityConfig.kt
@@ -1,60 +1,121 @@
 package no.nav.helse.navepjauthserver.security
 
 import com.nimbusds.jose.jwk.JWK
+import no.nav.helse.navepjauthserver.smart.FederatedIdentityIdTokenCustomizer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
-import org.springframework.security.config.Customizer
+import org.springframework.http.MediaType
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.oauth2.client.endpoint.NimbusJwtClientAuthenticationParametersConverter
+import org.springframework.security.oauth2.client.endpoint.RestClientAuthorizationCodeTokenResponseClient
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestCustomizers.withPkce
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig(
- @Value("\${helseid.openid.jwk}") private val helseidOpenJwk: String,
+  @Value("\${helseid.openid.jwk}") private val helseIdOpenidJwk: String
 ) {
-
-  private val helseIdJwk = JWK.parse(helseidOpenJwk)
 
   @Bean
   @Order(1)
   fun authorizationServerSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
-    val authConfig = OAuth2AuthorizationServerConfigurer()
+    val authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer()
     http {
-      securityMatcher(authConfig.endpointsMatcher)
-      with(authConfig) {
-        oidc { Customizer.withDefaults<OAuth2AuthorizationServerConfigurer>() }
+      securityMatcher(authorizationServerConfigurer.endpointsMatcher)
+
+      with(authorizationServerConfigurer) {
+        oidc { } // Enable OpenID Connect 1.0
       }
+
+      authorizeHttpRequests {
+        authorize(anyRequest, authenticated)
+      }
+
+      exceptionHandling {
+        defaultAuthenticationEntryPointFor(
+          LoginUrlAuthenticationEntryPoint("/oauth2/authorization/nav-epj-auth-server"),
+          MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+        )
+      }
+
       headers {
         frameOptions { sameOrigin }
       }
-
     }
     return http.build()
   }
 
   @Bean
   @Order(2)
-  fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+  fun securityFilterChain(
+    http: HttpSecurity,
+    repo: ClientRegistrationRepository,
+    tokenResponseClient: RestClientAuthorizationCodeTokenResponseClient,
+  ): SecurityFilterChain {
     http {
-      securityMatcher("/**")
       authorizeHttpRequests {
         authorize("/login/**", permitAll)
         authorize("/error/**", permitAll)
         authorize("/monitoring/**", permitAll)
         authorize(anyRequest, authenticated)
       }
+
+      oauth2Login {
+        authorizationEndpoint {
+          authorizationRequestResolver = pkceAddingResolver(repo)
+        }
+
+        tokenEndpoint {
+          accessTokenResponseClient = tokenResponseClient
+        }
+
+      }
+
       csrf { disable() }
-      cors { configurationSource = corsConfigurationSource()
+
+      cors {
+        configurationSource = corsConfigurationSource()
       }
     }
     return http.build()
   }
+
+  @Bean
+  fun oauth2TokenCustomizer(): OAuth2TokenCustomizer<JwtEncodingContext> {
+    return FederatedIdentityIdTokenCustomizer()
+  }
+
+  @Bean
+  fun tokenResponseClient(): RestClientAuthorizationCodeTokenResponseClient {
+    return RestClientAuthorizationCodeTokenResponseClient().apply {
+      setParametersConverter(NimbusJwtClientAuthenticationParametersConverter {
+        when (it.registrationId) {
+          "nav-epj-auth-server" -> JWK.parse(helseIdOpenidJwk)
+          else -> throw IllegalArgumentException("Unknown client: ${it.registrationId}")
+        }
+      })
+    }
+  }
+
+  private fun pkceAddingResolver(repo: ClientRegistrationRepository): DefaultOAuth2AuthorizationRequestResolver =
+    DefaultOAuth2AuthorizationRequestResolver(repo, "/oauth2/authorization").apply {
+      setAuthorizationRequestCustomizer(withPkce());
+    }
 
   private fun corsConfigurationSource(): CorsConfigurationSource {
     val config =
